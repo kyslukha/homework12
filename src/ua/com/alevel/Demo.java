87229bf5656c07c1dbbd4a3251156c84d6b75b9f
kyslukha <kyslukha@gmail.com>
2021-02-04 00:39:07 +0200

    third commit

--- src/ua/com/alevel/Demo.java
+++ src/ua/com/alevel/Demo.java
@@ -4,14 +4,13 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Demo {
-    public static String read(String path) {
+    public static String read(String path) throws IOException {
 //открываем файл по пути
-
-
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(path))) {
             StringBuilder stringBuilder = new StringBuilder();
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(path));
-                String currentString;
+
+            String currentString;
 //построчно считываем файл
             while ((currentString = reader.readLine()) != null) {
                 stringBuilder.append(currentString);
@@ -21,19 +20,13 @@ public class Demo {
             reader.close();
 //возвращаем вычитанный текст в строке
 
-        } catch (IOException e) {
-            System.out.println("IOException = " + e.getMessage());
-        } finally {
-                System.out.println("Finally");
-            }
-        return stringBuilder.toString();
+
+            return stringBuilder.toString();
+        }
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
         System.out.println(read("C:\\test.txt"));//в коде сразу была ошибка FileNotFoundEcxeption (в url были 2 пробела)пробела
-
-
     }
 
 }
