a0132f18f23ff25bda3698ca8e4f0f8b30798419
kyslukha <kyslukha@gmail.com>
2021-02-03 21:37:37 +0200

    first commit

---
+++ src/ua/com/alevel/Demo.java
@@ -0,0 +1,28 @@
+package ua.com.alevel;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Demo {
+    public static String read(String path) throws IOException {
+//открываем файл по пути
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        StringBuilder stringBuilder = new StringBuilder();
+        String currentString;
+//построчно считываем файл
+        while ((currentString = reader.readLine()) != null) {
+            stringBuilder.append(currentString);
+            stringBuilder.append(" \n ");
+        }
+//закрываем файл
+        reader.close();
+//возвращаем вычитанный текст в строке
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(read("C:\\test.txt"));//в коде сразу была ошибка FileNotFoundEcxeption (в url были 22 пробела)
+
+    }
+
+}
